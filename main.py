2129f01e344dc92b4c55b48995edab60f3e2f5f8
Edwin Boon <edwin.boon@eo.nl>
2022-07-22 08:29:03 +0200

    Added todo's for code improvement lesson

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
+
 def encrypt(text, shift):
   word_list = list(text)
   encypted_list = []
@@ -32,6 +34,8 @@ def decrypt(text, shift):
     decrypted_list.append(alphabet[new_position])
   
   print(f"The decoded text is {decrypted_word.join(decrypted_list)}")
+
+#TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
 
 if direction == 'encode':
   text = input("Type your message:\n").lower()
