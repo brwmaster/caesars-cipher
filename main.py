59665ea1409e272948c06023179cd67798bbb97b
Edwin Boon <edwin.boon@eo.nl>
2022-07-22 08:26:12 +0200

    Add decrypt function

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
 def encrypt(text, shift):
   word_list = list(text)
@@ -16,21 +13,35 @@ def encrypt(text, shift):
     if (new_position > len(alphabet)):
       new_position = round(new_position % len(alphabet))
 
-
     encypted_list.append(alphabet[new_position])
 
   print(f"The encoded text is {encrypted_word.join(encypted_list)}")
 
-#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(text, shift):
+  word_list = list(text)
+  decrypted_list =[]
+  decrypted_word = ''
 
-  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
-  #e.g. 
-  #cipher_text = "mjqqt"
-  #shift = 5
-  #plain_text = "hello"
-  #print output: "The decoded text is hello"
+  for char in word_list:
+    current_position = alphabet.index(char)
+    new_position = current_position - shift
 
+    if(new_position < 0):
+      new_position = len(alphabet) + (new_position)
 
-#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+    decrypted_list.append(alphabet[new_position])
+  
+  print(f"The decoded text is {decrypted_word.join(decrypted_list)}")
 
-encrypt(text, shift)
+if direction == 'encode':
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  encrypt(text, shift)
+elif direction == 'decode':
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  
+  decrypt(text, shift)
+else:
+  print(f"\nSorry the command {direction} is not valid.\nPlease use 'encode' or 'decode'")
