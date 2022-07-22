07d284c7df1c5bd5b6f01c8314b98c9c70b5845f
Edwin Boon <edwin.boon@eo.nl>
2022-07-22 10:09:40 +0200

    add some final touches to the code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from art import logo
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 
 def caesar(direction, text, shift):
   word_list = list(text)
@@ -10,23 +10,36 @@ def caesar(direction, text, shift):
     shift *= -1
 
   for char in word_list:
-    current_position = alphabet.index(char)
-
-    new_position = current_position + shift
-    
-    if (new_position > len(alphabet)):
-      new_position = round(new_position % len(alphabet))
-    elif (new_position < 0):
-      new_position = len(alphabet) + (new_position)
+    if char in alphabet:
+      current_position = alphabet.index(char)
+      new_position = current_position + shift
       
-    caesar_cipher.append(alphabet[new_position])
+      if (new_position > len(alphabet)):
+        new_position = round(new_position % len(alphabet))
+      elif (new_position < 0):
+        new_position = len(alphabet) + (new_position)
+        
+      caesar_cipher.append(alphabet[new_position])
+    else:
+      caesar_cipher.append(char)
 
-  print(f"The {direction}d text is {text.join(caesar_cipher)}")
+  print(f"The {direction}d text is: {text.join(caesar_cipher)}")
 
-if 'encode' in  direction or 'decode' in direction:
-  text = input("Type your message:\n").lower()
-  shift = int(input("Type the shift number:\n"))
+print(logo)
+quit_program = False
+while not quit_program:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 
-  caesar(direction, text, shift)
-else:
-  print(f"\nSorry the command {direction} is not valid.\nPlease use 'encode' or 'decode'")
+  if 'encode' in  direction or 'decode' in direction:
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(direction, text, shift)
+    user_input = input("\nType 'yes if you want to go agian. Otherwise type 'no' ").lower()
+
+    if(user_input == 'no'):
+      quit_program = True
+  else:
+    print(f"\nSorry the command {direction} is not valid.\nPlease use 'encode' or 'decode'")
+  
+ 
